da8d9c324fd228d420c4c974c647ffc80fdead4c
Marcos Oliveira <Marcos3601575>
2022-07-16 22:13:00 -0300

    Rename to Nerd Models

--- nerdvanapp/apps.py
+++ nerdvanapp/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NerdvanappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'nerdvanapp'
-    verbose_name = 'Models'
+    verbose_name = 'Nerd Models'
