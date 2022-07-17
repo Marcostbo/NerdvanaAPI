10142c0e00727df2ece7a59e3b6eb92267f6c6df
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-07-17 17:09:47 -0300

    build TwitchAPI script

--- nerdvanapp/scripts/games_twich_connection.py
+++ nerdvanapp/scripts/games_twich_connection.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 import requests
+import copy
 
 t = 1459987200
 
 date = datetime.utcfromtimestamp(t).strftime('%Y-%m-%d')
 
 consoles_ids = [169, 167, 130, 6, 49, 48, 12, 9]
-
-url = "https://api.igdb.com/v4/games?fields=name, summary, storyline, platforms, rating, rating_count, first_release_date&offset=0&limit=500"
 
 payload = {}
 files = {}
@@ -17,6 +16,39 @@ headers = {
   'Accept': 'application/json'
 }
 
-response = requests.request("GET", url, headers=headers, data=payload, files=files)
+full_games = []
+stop = 0
+limit = 500
+offset = 0
 
-print(response.text)
+while stop == 0:
+
+    url = f"https://api.igdb.com/v4/games" \
+          f"?fields=name,%20summary,%20storyline,%20platforms,%20rating,%20rating_count,%20first_release_date," \
+          f"%20involved_companies,%20category&limit={limit}&offset={offset}"
+
+    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+    games = response.json()
+
+    if not games:
+        stop = 1
+    full_games = full_games + games
+    offset = offset + 500
+
+full_games_copy = copy.deepcopy(full_games)
+filtered_games = []
+
+for game in full_games:
+    category = int(game.get("category"))
+    if category == 0:
+        platforms = game.get("platforms")
+        check = any(item in platforms for item in consoles_ids)
+        if check:
+            filtered_games.append(game)
+
+for game in filtered_games:
+    first_release_date = game.get('first_release_date')
+    if first_release_date:
+        t = abs(int(first_release_date))
+        game['first_release_date'] = datetime.utcfromtimestamp(t).strftime('%Y-%m-%d')
