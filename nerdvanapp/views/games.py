b944ef356447bb94490a45b8f999a3ec5ce7780b
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-07-27 21:23:36 -0300

    Fix

--- nerdvanapp/views/games.py
+++ nerdvanapp/views/games.py
@@ -1,6 +1,5 @@
 from django.http import Http404
 from rest_framework.exceptions import ValidationError
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from nerdvanapp.models import Games
