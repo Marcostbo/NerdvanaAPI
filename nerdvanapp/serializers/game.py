8247baa931ea03eb7c28881bbacbaecf68891085
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-02 18:41:18 -0300

    Validate method in GameQuerySerializer

--- nerdvanapp/serializers/game.py
+++ nerdvanapp/serializers/game.py
@@ -15,6 +15,15 @@ class GameQuerySerializer(serializers.Serializer):
     def create(self, validated_data):
         raise NotImplementedError
 
+    def validate(self, data):
+        game_filters = (data.get('name'), data.get('name_contains'), data.get('company_id'), data.get('console_id'))
+        number_of_filters = sum([0 if info is None else 1 for info in game_filters])
+        if number_of_filters > 1:
+            raise serializers.ValidationError('Specify only one filter')
+        if number_of_filters == 0:
+            raise serializers.ValidationError('Specify at least one filter')
+        return data
+
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
