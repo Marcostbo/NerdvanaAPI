2ebfd95755b911fbcff393b56d349f821a7c4a04
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-08 22:45:57 -0300

    Serializers for GameRecommenderView

--- nerdvanapp/views/recommender.py
+++ nerdvanapp/views/recommender.py
@@ -3,11 +3,12 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from nerdvanapp.models import Games
-from nerdvanapp.serializers import GameSerializer
+from nerdvanapp.serializers import GameSerializer, FullGameSerializer
 
 
 class GameRecommenderView(APIView):
     default_serializer = GameSerializer
+    serializers = (GameSerializer, FullGameSerializer)
 
     def get(self):
         request = self.request
