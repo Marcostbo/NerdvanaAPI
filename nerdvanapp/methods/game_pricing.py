bdeaa05d52e892624b1c093df9a4ddb100975c4a
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-28 21:47:43 -0300

    Define steps for game_pricing

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -1,11 +1,10 @@
 # Step 1: Define stores
 # 1.1 - Americanas
 # 1.2 - Submarino
-# 1.3 - Amazon
-# 1.4 - Kalunga
-# 1.5 - Kabum
-# 1.6 - Casas Bahia
-# 1.7 - Magalu
+# 1.3 - Ponto Frio (search as pontofrio)
+# 1.4 - Kabum
+# 1.5 - Casas Bahia
+# 1.6 - Magalu
 # Step 2: Search for game in Google via Webscraping
 # 2.1: Selenium?
 # Step 3: Get game price
