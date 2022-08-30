e56714a31a55f295b487aa81e7cf53f30b2912ce
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-29 22:11:37 -0300

    Start store model

---
+++ nerdvanapp/models/stores.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    search_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Store'
+        verbose_name_plural = 'Stores'
