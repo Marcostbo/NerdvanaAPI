a350576e7a9d63dcd342e8ed54f7eb656a7558d0
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-08-29 22:15:18 -0300

    Image as filefield (evaluate)

--- nerdvanapp/models/stores.py
+++ nerdvanapp/models/stores.py
@@ -4,6 +4,7 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length=60, unique=True)
     search_name = models.CharField(max_length=60, unique=True)
+    icon = models.FileField()
 
     def __str__(self):
         return self.name
