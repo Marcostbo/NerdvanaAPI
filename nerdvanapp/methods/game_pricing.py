8b247bc76f02410c8c026a75480dd68c4a6c147a
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-09-05 22:16:15 -0300

    Chromedriver and Webscrap with Selenium

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -8,3 +8,23 @@
 # Step 2: Search for game in Google via Webscraping
 # 2.1: Selenium?
 # Step 3: Get game price
+
+import urllib.parse as parse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+query = 'God of War PS4 Magalu'
+query = parse.quote_plus(query)
+
+google = 'https://www.google.com.br'
+url = 'https://www.google.com.br/search?q='
+search_url = url + query
+
+print(search_url)
+
+CLASS_NAME = 'T4OwTb'
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe')
+driver.get(search_url)
+
+results = driver.find_elements(By.CLASS_NAME, CLASS_NAME)
