2b8fc0aec3e032aac0c6344077743ca685e2cd1b
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-09-06 21:13:55 -0300

    Webscrap to get the url

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -5,6 +5,7 @@
 # 1.4 - Kabum
 # 1.5 - Casas Bahia
 # 1.6 - Magalu
+# 1.7 - Shoptime
 # Step 2: Search for game in Google via Webscraping
 # 2.1: Selenium?
 # Step 3: Get game price
@@ -13,7 +14,12 @@ import urllib.parse as parse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-query = 'God of War PS4 Magalu'
+# store = "https://www.magazineluiza.com.br"
+# store = "https://www.americanas.com.br"
+store = "https://www.shoptime.com.br/"
+# query = 'Elden Ring PS4 Magalu'
+# query = 'Elden Ring PS4 Americanas'
+query = 'Elden Ring PS4 Shoptime'
 query = parse.quote_plus(query)
 
 google = 'https://www.google.com.br'
@@ -28,3 +34,24 @@ driver = webdriver.Chrome(executable_path=r'C:\Users\Marcos Oliveira\PycharmProj
 driver.get(search_url)
 
 results = driver.find_elements(By.CLASS_NAME, CLASS_NAME)
+smaller_price = results[0].text
+
+page_source = results[0].parent.page_source
+
+start = f'href="{store}'
+end = '" data-agdh="arwt" id="vplap0"'
+
+
+def find_between(s, first, last):
+    try:
+        link_start = s.index(first) + len(first)
+        link_end = s.index(last, link_start)
+        return s[link_start:link_end]
+    except ValueError:
+        return ""
+
+
+link = find_between(page_source, start, end)
+full_link = store + link
+print(smaller_price)
+print(full_link)
