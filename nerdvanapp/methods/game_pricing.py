436a95e69313b93acce8d090a3b7fdd13bcc66ca
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-09-15 22:51:24 -0300

    Create GamePrincing class

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -1,59 +1,55 @@
-# Step 1: Define stores
-# 1.1 - Americanas
-# 1.2 - Submarino
-# 1.3 - Ponto Frio (search as pontofrio)
-# 1.4 - Kabum
-# 1.5 - Casas Bahia
-# 1.6 - Magalu
-# 1.7 - Shoptime
-# Step 2: Search for game in Google via Webscraping
-# 2.1: Selenium?
-# Step 3: Get game price
-
 import urllib.parse as parse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-store = "https://www.magazineluiza.com.br"
-# store = "https://www.americanas.com.br"
-# store = "https://www.shoptime.com.br"
-# store = "https://www.kabum.com.br"
-query = 'Elden Ring PS4 Magalu'
-# query = 'Elden Ring PS4 Americanas'
-# query = 'Elden Ring XBOX ONE Shoptime'
-# query = 'Elden Ring PS4 Kabum'
-query = parse.quote_plus(query)
 
-google = 'https://www.google.com.br'
-url = 'https://www.google.com.br/search?q='
-search_url = url + query
+class GamePricing:
+    def __init__(self, store_name, store_url, game, console):
+        self.google = 'https://www.google.com.br'
+        self.search = 'https://www.google.com.br/search?q='
+        self.class_name = 'T4OwTb'
+        self.start = f'href="{store_url}'
+        self.end = '" data-agdh="arwt" id="vplap0"'
 
-print(search_url)
+        query = game + ' ' + console + ' ' + store_name
+        self.store_url = store_url
+        self.query = parse.quote_plus(query)
+        self.search_url = self.search + self.query
 
-CLASS_NAME = 'T4OwTb'
+        self.smaller_price = None
+        self.full_link = None
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe')
-driver.get(search_url)
+    def get_smaller_price_and_url(self):
+        driver = webdriver.Chrome(
+            executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe'
+        )
+        driver.get(self.search_url)
 
-results = driver.find_elements(By.CLASS_NAME, CLASS_NAME)
-smaller_price = results[0].text
+        results = driver.find_elements(By.CLASS_NAME, self.class_name)
+        self.smaller_price = results[0].text
+        page_source = results[0].parent.page_source
 
-page_source = results[0].parent.page_source
+        link = self.find_between(page_source, self.start, self.end)
+        self.full_link = self.store_url + link
 
-start = f'href="{store}'
-end = '" data-agdh="arwt" id="vplap0"'
-
-
-def find_between(s, first, last):
-    try:
-        link_start = s.index(first) + len(first)
-        link_end = s.index(last, link_start)
-        return s[link_start:link_end]
-    except ValueError:
-        return ""
+    @staticmethod
+    def find_between(s, first, last):
+        try:
+            link_start = s.index(first) + len(first)
+            link_end = s.index(last, link_start)
+            return s[link_start:link_end]
+        except ValueError:
+            return ""
 
 
-link = find_between(page_source, start, end)
-full_link = store + link
-print(smaller_price)
-print(full_link)
+game_price = GamePricing(
+    store_name="Americanas",
+    store_url="https://www.americanas.com.br",
+    game="Elden Ring",
+    console="PS4"
+)
+
+game_price.get_smaller_price_and_url()
+print(game_price.smaller_price)
+print(game_price.full_link)
+
