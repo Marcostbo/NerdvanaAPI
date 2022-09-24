728ed0d58c1e57b5dfbf44e6afc9b4c4e883b8c1
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-09-23 21:19:16 -0300

    Search multiple stores

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 class GamePricing:
-    def __init__(self, store_name, store_url, game, console, stores_list=None):
+    def __init__(self, store_name, store_url, game, console):
         self.google = 'https://www.google.com.br'
         self.search = 'https://www.google.com.br/search?q='
         self.class_name = 'T4OwTb'
@@ -13,7 +13,7 @@ class GamePricing:
         self.end = '" data-agdh="arwt" id="vplap0"'
 
         query = game + ' ' + console + ' ' + store_name
-        self.stores_list = stores_list
+        self.stores_list = []
         self.store_url = store_url
         self.query = parse.quote_plus(query)
         self.search_url = self.search + self.query
@@ -42,11 +42,40 @@ class GamePricing:
         link = self.find_between(page_source, self.start, self.end)
         self.full_link = self.store_url + link
 
-    def get_smaller_price_and_url_for_multiple_stores(self):
+    def get_smaller_price_and_url_for_multiple_stores(self, game, console, stores_list):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-proxy-server')
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
         driver = webdriver.Chrome(
-            executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe'
+            executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe',
+            options=options
         )
-        # tomorrow: call the driver for each store
+        store_prices = []
+        for store in stores_list:
+            store_name = store[0]
+            store_url = store[1]
+            query = game + ' ' + console + ' ' + store_name
+            search_url = self.search + parse.quote_plus(query)
+            driver.get(search_url)
+
+            results = driver.find_elements(By.CLASS_NAME, self.class_name)
+            smaller_price = results[0].text
+            smaller_price = self.treate_price_string(smaller_price=smaller_price)
+
+            page_source = results[0].parent.page_source
+            link = self.find_between(page_source, self.start, self.end)
+            full_link = store_url + link
+            store_result = {
+                'Store': store_name,
+                'Price': smaller_price,
+                'Link': full_link
+            }
+            store_prices.append(store_result)
+
+        return store_prices
 
     @staticmethod
     def find_between(s, first, last):
@@ -67,16 +96,29 @@ class GamePricing:
 
 
 game_price = GamePricing(
-    store_name="Americanas",
-    store_url="https://www.americanas.com.br",
+    store_name="Shoptime",
+    store_url="https://www.shoptime.com.br",
     game="Elden Ring",
     console="PS4"
 )
+#
+# # import time
+# # start = time.time()
+# game_price.get_smaller_price_and_url()
+# # end = time.time()
+# # print(f'-- Total time {end - start}')
+# print(game_price.smaller_price)
+# print(game_price.full_link)
 
-import time
-start = time.time()
-game_price.get_smaller_price_and_url()
-end = time.time()
-print(f'-- Total time {end - start}')
-print(game_price.smaller_price)
-print(game_price.full_link)
+my_stores_list = [
+    ("Americanas", "https://www.americanas.com.br"),
+    ("Shoptime", "https://www.shoptime.com.br")
+]
+
+stores_result = game_price.get_smaller_price_and_url_for_multiple_stores(
+    game="Elden Ring",
+    console="PS4",
+    stores_list=my_stores_list
+)
+
+print(stores_result)
