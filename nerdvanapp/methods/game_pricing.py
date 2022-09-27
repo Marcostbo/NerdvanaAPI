c4fa8eafce3c352e9df34755af471811a4c119d3
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-09-27 20:41:51 -0300

    Refactor

--- nerdvanapp/methods/game_pricing.py
+++ nerdvanapp/methods/game_pricing.py
@@ -5,42 +5,11 @@ from selenium.webdriver.chrome.options import Options
 
 
 class GamePricing:
-    def __init__(self, store_name, store_url, game, console):
+    def __init__(self):
         self.google = 'https://www.google.com.br'
         self.search = 'https://www.google.com.br/search?q='
         self.class_name = 'T4OwTb'
-        self.start = f'href="{store_url}'
         self.end = '" data-agdh="arwt" id="vplap0"'
-
-        query = game + ' ' + console + ' ' + store_name
-        self.stores_list = []
-        self.store_url = store_url
-        self.query = parse.quote_plus(query)
-        self.search_url = self.search + self.query
-
-        self.smaller_price = None
-        self.full_link = None
-
-    def get_smaller_price_and_url(self):
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--no-proxy-server')
-        options.add_argument("--proxy-server='direct://'")
-        options.add_argument("--proxy-bypass-list=*")
-        driver = webdriver.Chrome(
-            executable_path=r'C:\Users\Marcos Oliveira\PycharmProjects\NerdvanaAPI\chromedriver.exe',
-            options=options
-        )
-        driver.get(self.search_url)
-
-        results = driver.find_elements(By.CLASS_NAME, self.class_name)
-        smaller_price = results[0].text
-        self.smaller_price = self.treate_price_string(smaller_price=smaller_price)
-
-        page_source = results[0].parent.page_source
-        link = self.find_between(page_source, self.start, self.end)
-        self.full_link = self.store_url + link
 
     def get_smaller_price_and_url_for_multiple_stores(self, game, console, stores_list):
         options = Options()
@@ -96,21 +65,7 @@ class GamePricing:
         return float(price)
 
 
-game_price = GamePricing(
-    store_name="Shoptime",
-    store_url="https://www.shoptime.com.br",
-    game="Elden Ring",
-    console="PS4"
-)
-
-#
-# # import time
-# # start = time.time()
-# game_price.get_smaller_price_and_url()
-# # end = time.time()
-# # print(f'-- Total time {end - start}')
-# print(game_price.smaller_price)
-# print(game_price.full_link)
+game_price = GamePricing()
 
 my_stores_list = [
     ("Americanas", "https://www.americanas.com.br"),
