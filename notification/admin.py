5702f004f9b6d3532e38518f9fb84ae1a9d25d0d
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-10-11 16:35:23 -0300

    Add SentNotification model in admin

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SentNotification
 
-# Register your models here.
+
+class SentNotificationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'recipient', 'reason']
+    search_fields = ['id', 'reason']
+    list_filter = ('reason',)
+
+
+admin.register(SentNotification, SentNotificationAdmin)
+
---
+++ notification/models/__init__.py
@@ -0,0 +1,1 @@
+from .sent_notification import SentNotification
