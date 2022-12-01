d6af6bcd13e6150691f0cbc69cabbabbff870c7d
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2022-12-01 20:19:41 -0300

    Create PriceAlert model

--- nerdvanapp/models/__init__.py
+++ nerdvanapp/models/__init__.py
@@ -3,3 +3,4 @@ from .console import Console, Company, Family
 from .games import Games
 from .game_company import GameCompany
 from .stores import Store
+from .price_alert import PriceAlert
---
+++ nerdvanapp/models/price_alert.py
@@ -0,0 +1,20 @@
+from django.db import models
+from nerdvanapp.models import User, Games
+
+
+class PriceAlert(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=8, null=False)
+    created_on = models.DateTimeField()
+    resolved_on = models.DateTimeField()
+    is_resolved = models.BooleanField(default=False)
+    price_resolved = models.DecimalField(decimal_places=2, max_digits=8)
+    link_resolved = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = 'Price alert'
+        verbose_name_plural = 'Price alerts'
+
+    def __str__(self):
+        return f'Alert for {self.game.name} with price R${self.price} to {self.user.email}'
