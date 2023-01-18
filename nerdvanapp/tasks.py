3586df59492fef384edcacb1e2a0f5dd45a0c860
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-01-18 18:32:51 -0300

    Evaluate price inside task

--- nerdvanapp/tasks.py
+++ nerdvanapp/tasks.py
@@ -3,6 +3,12 @@ from nerdvanapp.models import User, Games, PriceAlert
 from nerdvanapp.methods.game_pricing import GamePricing
 
 
+def evaluate_price(price_limit: float, current_price: float):
+    if current_price < price_limit:
+        return True
+    return False
+
+
 @shared_task
 def test_celery():
     """ This task evaluates all the price alerts create
@@ -12,11 +18,16 @@ def test_celery():
     stores_list = ['Americanas', 'Kabum']
     for price_alert in price_alerts:
         # evaluate the alert
-        GamePricing().get_smaller_price_and_url_for_multiple_stores(
+        game_prices = GamePricing().get_smaller_price_and_url_for_multiple_stores(
             game=price_alert.game.name,
             console='PS4',
             stores_list=stores_list
         )
+        for game_price in game_prices:
+            is_resolved = evaluate_price(
+                price_limit=price_alert.price,
+                current_price=game_price.get('price')
+            )
 
     user = User.objects.get(id=1)
     game = Games.objects.get(id=16516)
