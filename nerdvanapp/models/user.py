6c3ba16e210369dad4cba753079e438913165458
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-03-09 21:15:32 -0300

    Add migration to fix registration date

--- nerdvanapp/models/user.py
+++ nerdvanapp/models/user.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    validated_on = models.DateTimeField()
+    validated_on = models.DateTimeField(null=True)
     email_validated = models.BooleanField(default=False)
     password_changed = models.BooleanField(default=False)
     username = None
