7dd0770815bcfe880874d6f1f307b5700b5b44b1
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-06-14 00:00:25 -0300

    Register first_name and last_name for new user

--- nerdvanapp/views/register.py
+++ nerdvanapp/views/register.py
@@ -6,7 +6,13 @@ from rest_framework.response import Response
 class RegisterView(APIView):
     @staticmethod
     def post(request):
-        user = UserSerializer(data=request.data)
+        # Build user input data
+        user_data = request.data.copy()
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        user_data['name'] = f'{first_name} {last_name}'
+        # Serialize and create user
+        user = UserSerializer(data=user_data)
         user.is_valid(raise_exception=True)
         user.save()
 
