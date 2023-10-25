3200196d7c7d7f347deac471e46d2a293717a728
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-10-25 17:31:58 -0300

    Create new tasks and workers: celery-email and celery-pricing

--- nerdvana/celery.py
+++ nerdvana/celery.py
@@ -4,10 +4,16 @@ from django.conf import settings
 from celery.schedules import crontab
 from celery import Celery
 
-# Celery Settings
+# Celery General Settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nerdvana.settings")
 app = Celery("nerdvana", broker=settings.CELERY_BROKER_URL)
+
+# Celery Tasks Settings
+
+app.conf.task_routes = {
+    'notification.tasks.send_email': {'queue': 'email_sending'}
+}
 
 # Celery Beat Settings
 
@@ -15,6 +21,7 @@ app.conf.beat_schedule = {
     "new_task": {
         "task": "nerdvanapp.tasks.evaluate_price_alerts",
         "schedule": crontab(minute="*/3"),
+        "options": {"queue": "game_pricing"}
     },
 }
 
